23aa60400dc4298652d3863e8a112cb38dfcede6
Amith K A <amith.ka@intimetec.com>
2021-06-26 15:46:25 +0530

    added firebase config files

---
+++ src/shared/utility/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCVyO2JkjrmH5TcCneJK3TnhJBcavu0LaY",
+  authDomain: "pro-x-2395c.firebaseapp.com",
+  projectId: "pro-x-2395c",
+  storageBucket: "pro-x-2395c.appspot.com",
+  messagingSenderId: "991750913774",
+  appId: "1:991750913774:web:48713e7aa25dfb94ea7dff",
+  measurementId: "G-YMSHB4414Y",
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+export default app;
