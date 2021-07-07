afb9a38d4202a6854eebe3f22a95ac5408959786
Amith K A <amith.ka@intimetec.com>
2021-07-07 12:36:53 +0530

    Added mock api to cart context

--- src/__test__/pages/home/home.test.js
+++ src/__test__/pages/home/home.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "../../../pages/home/home";
 import ReactDOM from "react-dom";
 import { render, act, cleanup } from "@testing-library/react";
+import { Switch, BrowserRouter } from "react-router-dom";
 
 afterEach(cleanup);
 
@@ -10,13 +11,26 @@ describe("For Access Screen", () => {
 
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Home />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <Switch>
+          <Home />
+        </Switch>
+      </BrowserRouter>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it("Should have Title in Home Screen", async () => {
     await act(async () => {
-      const wrapped = render(<Home />);
+      const wrapped = render(
+        <BrowserRouter>
+          <Switch>
+            <Home />
+          </Switch>
+        </BrowserRouter>
+      );
 
       const title = wrapped.getByText("Buy 1, Get 3 Free");
       expect(title).not.toBeNull();
